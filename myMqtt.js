abfaa4ea6ea803fb9fee0d6d3cec2a359230751d
AvirutRut <60030112@kmitl.ac.th>
2020-11-24 21:02:20 +0700

    add

---
+++ myMqtt.js
@@ -0,0 +1,29 @@
+var mqtt=require('mqtt');
+    
+    const MQTT_SERVER="localhost";
+    const MQTT_PORT="1883";
+    //if your server don't have username and password leave it blank
+    const MQTT_USER="Avirut";
+    const MQTT_PASSWORD="123456";
+
+    var client=mqtt.connect({
+        host: MQTT_SERVER,
+        port: MQTT_PORT,
+        username: MQTT_USER,
+        password: MQTT_PASSWORD
+    });
+
+    client.on('connect',()=>{
+        console.log("MQTT Connected");
+        client.subscribe('test',(err)=>{ //subscribe to 'test' topic
+            if(err)console.log(err);
+        });
+    });
+
+    client.on('message',(topic,message)=>{
+        console.log(topic.toString()+" : "+message.toString());
+    });
+
+    setInterval(()=>{
+        client.publish('test','hello from NodeJS'); //publish "hello from NodeJS" to "test" topic every 5 second
+    },5000);
